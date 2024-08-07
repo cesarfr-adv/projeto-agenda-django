9363bb003dd5f94a64b184c34ca3335b9b0fc495
cesarfr-adv <cesarfr.adv@gmail.com>
2024-08-07 14:16:40 -0300

    Implantação da Model contact 	modified:   contact/admin.py

--- contact/admin.py
+++ contact/admin.py
@@ -6,4 +6,4 @@ from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'first_name', 'last_name', 'phone',
-    ordering =
+    ordering = 'id'
